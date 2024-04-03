8cac75c4006d935800d8d09cf0c56b7a09ae64a2
BrunoCsr <bruno.c@geb.inatel.br>
2024-04-02 23:59:41 -0300

    added the secondary pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:asd_project_app/screens/ai_page.dart';
+import 'package:asd_project_app/screens/kids.dart';
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
 
@@ -9,10 +11,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'REST - ASD',
       home: HomeScreen(title: 'ASD'),
+     routes: <String, WidgetBuilder>{
+        '/a': (BuildContext context) => const HomeScreen(title: "Home",),
+        '/b': (BuildContext context) => const KidsPage(),
+        '/c': (BuildContext context) => const AIPage(),
+      },
     );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,3 +1,5 @@
+import 'package:asd_project_app/screens/ai_page.dart';
+import 'package:asd_project_app/screens/kids.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,6 +10,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildTextComposer(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        ElevatedButton.icon(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const KidsPage()),
+            );
+          },
+          icon: const Icon(Icons.child_care),
+          label: const Text("Composição de alunos"),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.black),
+              foregroundColor: MaterialStateProperty.all(Colors.white)),
+        ),
+        ElevatedButton.icon(
+          onPressed: () {
+           Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const AIPage()),
+            );
+          },
+          icon: const Icon(Icons.wechat),
+          label: const Text("Converse com a IA"),
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.black),
+              foregroundColor: MaterialStateProperty.all(Colors.white)),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +52,22 @@ class _HomeScreenState extends State<HomeScreen> {
           title: const Text('REST ASD'),
           centerTitle: true,
         ),
-        );
+        body: SafeArea(
+          child: Column(
+            children: [
+              Flexible(
+                  child: Container(
+                height: MediaQuery.of(context).size.height,
+              )),
+              Container(
+                decoration: const BoxDecoration(color: Colors.white),
+                child: _buildTextComposer(context),
+              ),
+              const SizedBox(
+                height: 30,
+              )
+            ],
+          ),
+        ));
   }
 }
-
